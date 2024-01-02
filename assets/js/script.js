5477c3483272508ba1ea2d123e0fd0023751ee99
Bruno Nolasco <brunosocialmedia150769@gmail.com>
2024-01-02 17:14:49 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu()
+{
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.getElementById('navBar');
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}       
